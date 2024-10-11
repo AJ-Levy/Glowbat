6be59781761795378136ea6a07344589a91cc0ab
Ariel J. Levy <ariellevy54@gmail.com>
2024-10-11 10:08:42 +0200

    Made firefly nice and hopefully stopped it clipping into floor/roof.

--- Assets/Code/FireflyAI.cs
+++ Assets/Code/FireflyAI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class FireflyAI : MonoBehaviour
@@ -12,6 +13,9 @@ public class FireflyAI : MonoBehaviour
     private Light2D fireflyLight;
     private float minIntensity = 0.2f;
     private float maxIntensity = 1.5f;
+    private float minOuterRadius = 0.1f;
+    private float maxOuterRadius = 0.4f;
+    private float innerRadiusFactor = 0.4f;
     private float flickerSpeed = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,7 +23,7 @@ public class FireflyAI : MonoBehaviour
     {
         originalYPos = transform.position.y;
         mainCamera = Camera.main; 
-        fireflyLight = gameObject.GetComponent<Light2D>();
+        fireflyLight = gameObject.GetComponentInChildren<Light2D>();
     }
 
     // Update is called once per frame
@@ -30,12 +34,14 @@ public class FireflyAI : MonoBehaviour
         float newY = originalYPos + Mathf.Sin(Time.time) * heightVariation;
         transform.position = new Vector3(transform.position.x, newY, transform.position.z); 
 
-        if (mainCamera.transform.position.x - transform.position.x >  maxDistanceFromCamera)
+        if (mainCamera.transform.position.x - transform.position.x > maxDistanceFromCamera)
         {
             Destroy(gameObject); // Destroy the firefly
         }
 
-        // Simulate flickering by varying intensity
+        // Simulate flickering by varying intensity and light size
         fireflyLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, Mathf.PingPong(Time.time * flickerSpeed, 1));
+        fireflyLight.pointLightInnerRadius = Mathf.Lerp(innerRadiusFactor * minOuterRadius, innerRadiusFactor * maxOuterRadius, Mathf.PingPong(Time.time * flickerSpeed, 1));
+        fireflyLight.pointLightOuterRadius = Mathf.Lerp(minOuterRadius, maxOuterRadius, Mathf.PingPong(Time.time * flickerSpeed, 1));
     }
 }
